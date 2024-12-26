93202a61d9ce9bdc56e3c9ccd0242abf27f5699d
KarimSt23 <mohamed.karim.satouri@gmail.com>
2024-12-26 22:34:07 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to my Flask app!"
+
+@app.route("/about")
+def about():
+    return "This is a simple Flask application with two routes."
+
+if __name__ == "__main__":
+    app.run(debug=True)
