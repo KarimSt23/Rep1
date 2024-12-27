1db44547ab7e06a71ba3739660f9d1edaa31f605
KarimSt23 <mohamed.karim.satouri@gmail.com>
2024-12-27 01:53:53 +0100

    oss commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return "This is a simple Flask application with two routes. Hyyyy hyyy"
+    return "This is a simple Flask application with two routes. Hyyyy ossama"
 
 if __name__ == "__main__":
     app.run(debug=True)
